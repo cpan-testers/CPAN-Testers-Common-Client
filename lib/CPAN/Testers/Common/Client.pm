c2909a76c676c6392d6a761e570fbcc7c6292b20
Breno G. de Oliveira <garu@cpan.org>
2013-05-06 03:47:30 -0300

    bumping up version and changes

--- lib/CPAN/Testers/Common/Client.pm
+++ lib/CPAN/Testers/Common/Client.pm
@@ -13,7 +13,7 @@ use CPAN::Testers::Common::Client::PrereqCheck;
 
 use constant MAX_OUTPUT_LENGTH => 1_000_000;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 
 #==================================
